de6393c7b4220318399de13d6852ecd04801b75a
mohamedasar11111 <65416377+mohamedasar11111@users.noreply.github.com>
2024-10-23 13:35:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
-import streamlit as st
+import torch
+from transformers import pipeline, TrOCRProcessor, VisionEncoderDecoderModel
+from datasets import load_dataset
+import soundfile as sf
+from PIL import Image
+from IPython.display import display
+import pytesseract
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-large-handwritten")
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-large-handwritten")
+
+def show_image(path_str):  # Use snake_case for function names
+    img = Image.open(path_str).convert("RGB")
+    display(img)
+    return img
+
+def ocr_image(src_img):
+    return pytesseract.image_to_string(src_img)
+
+picture_text = show_image('./handwriting.png')  # Use snake_case for variable names
+exported_text = ocr_image(picture_text)
+print(exported_text)
+
+synthesizer = pipeline("text-to-speech", "microsoft/speecht5_tts") 
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+
+speech = synthesizer(exported_text, forward_params={"speaker_embeddings": speaker_embedding})
+sf.write("exported_text.wav", speech["audio"], samplerate=speech["sampling_rate"])
