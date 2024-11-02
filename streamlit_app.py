b0550facdbdcbb0669e250b9ba8bae2419d358d3
mohamedasar11111 <65416377+mohamedasar11111@users.noreply.github.com>
2024-11-02 21:41:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,27 +3,33 @@ from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
 import warnings
 warnings.filterwarnings("ignore")
-# Load the TrOCR processor and model
+
 processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
 def load_model():
     model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
     return model
 
 model = load_model()
-# Set up the Streamlit app
+
 st.title("TrOCR for Image-to-Text")
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Open the uploaded image
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image")
+     try:
+        image = Image.open(uploaded_file).convert("RGB")
+        st.image(image, caption="Uploaded Image")
+        pixel_values = processor(image, return_tensors="pt").pixel_values
+        generated_ids = model.generate(pixel_values)
+        extracted_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        st.subheader("Extracted Text:")
+        st.write(extracted_text)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
-    # Process the image with TrOCR
     pixel_values = processor(image, return_tensors="pt").pixel_values
     generated_ids = model.generate(pixel_values)
     extracted_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
-    # Display the extracted text
+
     st.subheader("Extracted Text:")
     st.write(extracted_text)
